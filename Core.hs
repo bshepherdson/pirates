cd0989f4806162968666600f36593b37da7437d9
Braden Shepherdson <Braden.Shepherdson@gmail.com>
2008-09-20 13:05:32 -0400

    Network infrastructure, slightly reworked Ships. No command parsing yet.
    darcs-hash:20080920170532-d53a8-7ac4ed60960a69df410730482643a524828e6112.gz

--- Core.hs
+++ Core.hs
@@ -1,5 +1,205 @@
+{-# LANGUAGE 
+BangPatterns,
+GeneralizedNewtypeDeriving #-}
 
 module Main where
+
+import System.IO
+import System.Exit
+import System.Random
+import System.Environment
+import Network
+
+import Control.Monad
+import Control.Monad.STM
+import Control.Monad.State
+import Control.Monad.Trans
+import Control.Monad.Error
+import Control.Applicative
+import Control.Arrow hiding (loop)
+
+import Control.Concurrent
+import Control.Concurrent.STM.TChan
+
+import Data.List
+import Data.Char
+
+import Ship
+
+
+type ClientId = Int
+type MasterChannel = TChan Event
+
+data Event = Message (ClientId,String)
+           | NewClient Client
+           | Disconnect ClientId
+           | NewReader ClientId ThreadId
+
+data Client = Client {
+      cid        :: !ClientId
+    , socket     :: !Handle
+    , chan       :: TChan String
+    , reader     :: !ThreadId
+    , writer     :: !ThreadId
+    , ship       :: Ship
+    }
+
+
+data PState = PState { clients  :: [Client]
+                     , inChan   :: MasterChannel
+                     }
+
+newtype P a = P (StateT PState IO a)
+    deriving (Functor, Monad, MonadIO, MonadState PState)
+
+
+io = liftIO
+
+
+readerThread :: ClientId -> Handle -> MasterChannel -> IO ()
+readerThread c h chan = forever $ do
+                          catch (hGetLine h >>= \s ->
+                                     atomically $ writeTChan chan (Message (c,s)))
+                                (const $ hClose h >> putDisconnect c chan)
+
+
+writerThread :: ClientId -> Handle -> TChan String -> MasterChannel -> IO ()
+writerThread c h chan mchan 
+    = forever $ do
+        s <- atomically $ readTChan chan
+        catch (hPutStrLn h s >> hFlush h)
+              (const $ hClose h >> putDisconnect c mchan)
+
+
+putDisconnect :: ClientId -> MasterChannel -> IO ()
+putDisconnect c chan = do
+  putStrLn $ "Disconnecting client " ++ show c
+  atomically $ writeTChan chan (Disconnect c)
+
+
+
+
+------------------------------------------
+------- utility sending functions --------
+------------------------------------------
+
+send :: String -> Client -> P ()
+send s c = io $ atomically $ writeTChan (chan c) s
+
+to :: ClientId -> String -> P ()
+to c s = findClient c >>= send s
+
+toAll :: String -> P ()
+toAll s = gets clients >>= mapM_ (send s)
+
+toAllBut :: ClientId -> String -> P ()
+toAllBut c s = gets clients >>= mapM_ (send s) . filter ((/=c).cid)
+
+
+
+--------------- helper functions ------------------------
+
+findClient :: ClientId -> P Client
+findClient c = do
+  cs <- gets clients
+  case find ((==c).cid) cs of
+    Nothing -> do
+      io $ putStrLn $ "The impossible just happened: ClientId " ++ show c ++ " does not exist."
+      io $ exitWith (ExitFailure 1)
+    Just c' -> return c'
+
+
+
+-- warning: partial function
+updateMatching :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+updateMatching p f xs = pre ++ f x' : xs'
+    where (pre, (x':xs')) = break p xs
+
+updateClient :: ClientId -> (Client -> Client) -> P ()
+updateClient c f = modify (\st -> st { clients = updateMatching ((==c).cid) f (clients st) })
+
+
+
+maybeRead :: (Read a) => String -> Maybe a
+maybeRead s = case reads s of
+                [(y,"")] -> Just y
+                _        -> Nothing
+
+
+
+
+
+-----------------------------------------
+-------------   commands    -------------
+-----------------------------------------
+
+parser = undefined
+
+
+
+
+
+------------------------------------------------
+-------------- server functions ----------------
+------------------------------------------------
+
+
+runP :: PState -> P a -> IO (a, PState)
+runP st (P a) = runStateT a st
+
+loop :: MasterChannel -> P ()
+loop chan = forever $ do
+              e <- io $ atomically $ readTChan chan
+              case e of
+                Message cs -> parser cs
+                NewClient c -> do
+                        st <- get
+                        let cs = clients st
+                        put $ st { clients = c:cs }
+                Disconnect c -> do
+                        st <- get
+                        let cs = clients st
+                        case break ((==c).cid) cs of
+                          (_,[]) -> return () -- do nothing, client already gone
+                          (pre,d:post) -> do
+                                  io $ killThread $ writer d
+                                  tid <- io $ myThreadId
+                                  if reader d == tid
+                                    then return () -- do nothing.
+                                    else io $ killThread $ reader d
+                                  put st { clients = pre ++ post }
+                                  toAll $ "System: Client Disconnected: " ++ show (cid d)
+                NewReader c t -> updateClient c $ \c -> c { reader = t }
+
+
+
+main = do
+  args <- getArgs
+  case args of
+    (port:_) -> do
+              chan <- atomically newTChan
+              tid  <- forkIO $ runP (PState [] chan) (loop chan) >> return ()
+              sock <- listenOn (PortNumber (fromIntegral ((read port)::Int)))
+              serverLoop chan sock tid 0
+    [] -> putStrLn "Usage: pirateserver <port>"
+
+
+serverLoop :: MasterChannel -> Socket -> ThreadId -> ClientId -> IO ()
+serverLoop chan s tid c = do
+  (h,_,_) <- accept s
+  nc <- atomically newTChan
+  putStrLn $ "New client! Assigning cid "++ show c
+  wid <- forkIO $ writerThread c h nc chan
+  atomically $ writeTChan chan $ NewClient (Client {
+                                              cid    = c
+                                            , socket = h
+                                            , chan   = nc
+                                            , reader = tid
+                                            , writer = wid
+                                            })
+  rid <- forkIO $ readerThread c h chan
+  atomically $ writeTChan chan (NewReader c rid)
+  serverLoop chan s tid $! c+1
 
 
 
--- Ship.hs
+++ Ship.hs
@@ -12,10 +12,10 @@ type Speed   = Int -- measured in yards per tick
 
 -- transforms wind heading and speed, given ships' heading, 
 -- into a new heading and speed for the ship at the specified sail setting
-type SailFunc :: Heading -> Speed -> Heading -> (Heading, Speed)
+type SailFunc = Heading -> Speed -> Heading -> (Heading, Speed)
 
 -- maps user sail setting commands to functions that embody them for this ship
-data Sail = M.Map String SailFunc
+type Sails = M.Map String SailFunc
 
 -- just a name, for now.
 type Captain = String
@@ -25,7 +25,7 @@ data Ship = Ship {
     , name         :: !String
     , captain      :: !Captain
     , course       :: !Heading
-    , sails        :: !Sail
+    , sails        :: !Sails
     , sail         :: !SailFunc
     }
 
@@ -40,29 +40,40 @@ tpFrigate = Ship {
             , captain   = "Myron Scrant"
             , course    = 0
             , sails     = ssFrigate
-            , sail      = sFurled
+            , sail      = furled
             }
 
 
 ssFrigate :: Sails
 ssFrigate = M.fromList [
-             ("full"    , sLargeSquare)
-            ,("drawn"   , second q3 . sLargeSquare)
-            ,("half"    , second q2 . sLargeSquare)
+             ("full"    , sLargeFastSquare)
+            ,("drawn"   , sq3 sLargeFastSquare)
+            ,("half"    , sq2 sLargeFastSquare)
+            ,("light"   , sq1 sLargeFastSquare)
             ,("steerage", steerage)
             ,("furled"  , furled)
             ]
 
 
 
-q3 :: Speed -> Speed
-q3 x = (x*3) `div` 4
+-- extremely crude: 
+-- 0-22.5* -> 0
+-- 22.5-45 -> 1/4
+-- 45-90   -> 1/2
+-- 90-120  -> 3/4
+-- 120-180 -> 1
+-- unlike SMP, ships here are limited to the wind speed.
+sLargeFastSquare :: SailFunc
+sLargeFastSquare wh ws sh | dh < 23   = (sh,0)
+                          | dh < 45   = (sh,q1 ws)
+                          | dh < 90   = (sh,q2 ws)
+                          | dh < 120  = (sh,q3 ws)
+                          | otherwise = (sh,ws)
+    where dh = windDiff wh sh
 
-q2 :: Speed -> Speed
-q2 x = x `div` 2
-
-q1 :: Speed -> Speed
-q1 x = x `div` 4
+-----------------------------
+-- ship-independent speeds --
+-----------------------------
 
 steerage :: SailFunc
 steerage _ _ sh = (sh, 1)
@@ -70,6 +81,34 @@ steerage _ _ sh = (sh, 1)
 
 -- moves one degree closer to the wind, either with or into it.
 furled :: SailFunc
-furled wh ws sh | an == 90  = (sh,0)
-                | otherwise = (sh + signum (wh-sh), 0)
+furled wh ws sh | windDiff wh sh == 90  = (sh,0)
+                | otherwise             = (sh + signum (wh-sh), 0)
+
+
+
+
+-----------------------------
+----- utility functions  ----
+-----------------------------
+
+-- returns a value 0 <= x <= 180 describing the absolute difference between
+-- the wind and the ship.
+windDiff :: Heading -> Heading -> Heading
+windDiff wh sh = norm180 $ abs $ sh - wh
+    where norm180 x | x >= 360 = norm180 $ x - 360
+                    | x <  0   = norm180 $ x + 360
+                    | x >  180 = 180 - (x - 180)     -- weird but correct
+
+
+sq3,sq2,sq1 :: SailFunc -> SailFunc
+sq3 f wh ws sh = second q3 $ f wh ws sh
+sq2 f wh ws sh = second q2 $ f wh ws sh
+sq1 f wh ws sh = second q1 $ f wh ws sh
+
+
+q3,q2,q1 :: Speed -> Speed
+q3 x = (x * 3) `div` 4
+q2 = (`div`2)
+q1 = (`div`4)
+
 
